b2a7e06519b2a349bce9fc6e44925a1af3f0a109
fab9410 <r.rayan200310@gmail.com>
2023-06-05 18:50:57 +0200

    generatepdf

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProfRepository;
 use App\Repository\EleveRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(EleveRepository $eleveRepository): Response
+    public function index(EleveRepository $eleveRepository, ProfRepository $profRepository): Response
     {
         $nbEleve = count($eleveRepository->findAll());
+        $nbProf= count($profRepository->findAll());
         return $this->render('home/index.html.twig', [
             'nbEleve' => $nbEleve,
+            'nbEleve' => $nbProf,
         ]);
     }
 }
